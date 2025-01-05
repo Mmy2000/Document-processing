64b4834519183f4e04d555489641b2aedd7e1f82
Mmy2000 <mm.yousef811@gmail.com>
2025-01-06 00:38:08 +0200

    add models for api app and create urls and serializers files

--- DPS/api/admin.py
+++ DPS/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UploadedFile, ImageDetails, PDFDetails
 # Register your models here.
+
+admin.site.register(UploadedFile)
+admin.site.register(ImageDetails)
+admin.site.register(PDFDetails)
--- DPS/api/models.py
+++ DPS/api/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class UploadedFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    FILE_TYPES = [("image", "Image"), ("pdf", "PDF")]
+    file_type = models.CharField(max_length=10, choices=FILE_TYPES)
+    file_path = models.FileField(upload_to="uploads/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file_path.name
+
+
+class ImageDetails(models.Model):
+    file = models.OneToOneField(UploadedFile, on_delete=models.CASCADE)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    channels = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.width}x{self.height}x{self.channels}"
+
+
+class PDFDetails(models.Model):
+    file = models.OneToOneField(UploadedFile, on_delete=models.CASCADE)
+    pages = models.IntegerField()
+    page_width = models.FloatField()
+    page_height = models.FloatField()
+
+    def __str__(self):
+        return f"{self.pages} pages"
